7242ce4dcd8d85ec33856c126465787a6b3dad07
master <alya26271@gmail.com>
2023-12-04 23:47:40 +0200

    PhoneValidator

--- src/PhoneValidatorTest.java
+++ src/PhoneValidatorTest.java
@@ -3,21 +3,13 @@ import java.io.*;
 public class PhoneValidatorTest {
 //    public static void main(String[] args) {
 //        String phones = "987-123-4567\n" +    //true
-//                    "\n" +
 //                    "123 456 7890\n" +
-//                    "\n" +
 //                    "(123) 456-7890\n" +      //true
-//                    "\n" +
 //                    "127-233-0987\n" +        //true
-//                    "\n" +
 //                    "987-876 7890\n" +
-//                    "\n" +
 //                    "(783) 123-5678\n" +      //true
-//                    "\n" +
 //                    "(783)-123-5678\n" +
-//                    "\n" +
 //                    "4129-233-0987t\n" +
-//                    "\n" +
 //                    "777-555-1234";            //true
 //        try(FileOutputStream fileOutputStream = new FileOutputStream("file.txt")) {
 //            byte[] buffer = phones.getBytes();
